a58bf5b1d210d0469658681d6fae63044fca93a9
Stefano <67341828+stefanoturcarelli@users.noreply.github.com>
2024-06-12 10:32:45 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# React Params
+
+Syncronize the URL params with the React state.
+
+The value of the state will be also updated when the URL params change. So,
+you can copy the URL and share it with someone else and the state will be
+updated automatically.
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <main>
+        <div className="container">
+          <h1>React Params</h1>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
